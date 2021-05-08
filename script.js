29d815256f5d25e44fcaf26e53f8a9f0b81873bc
Barrack Roy <68614929+barrack81@users.noreply.github.com>
2021-05-08 17:33:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Desinger", " Web Developer", "UX UI Designer", "Freelancer"],
+        strings: ["Web Desinger", " Web Developer", "Network Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Desinger", " Web Developer", "UX UI Designer", "Freelancer"],
+        strings: ["Web Desinger", " Web Developer", "Network Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
