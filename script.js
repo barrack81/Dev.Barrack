2492a0efeda61635cb35412b86b4f51e1919a577
Barrack Roy <68614929+barrack81@users.noreply.github.com>
2021-11-21 00:30:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cloud Engineer", " Web Developer", "Network Expert", "Data Scientist"],
+        strings: ["Cloud Engineer", " Web Developer", "MFC Tutor", "AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud Engineer", " Web Developer", "Network Expert", "Data Scientist"],
+        strings: ["Cloud Engineer", " Web Developer", "MFC Tutor", "AI Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
